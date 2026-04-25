b88dbef0b5089a0a92c32d8fb6dc523413235df4
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-04-25 18:16:00 +0100

    feat: deleted secondary component     - added base style as a const     - added individual styles to an array     - mapped styles to button component     - reduced the icon size

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,25 +1,22 @@
 function Button(props) {
-  return (
-    <button onClick={props.onClick} className="cursor-pointer bg-primary flex items-center justify-center gap-2 rounded-full h-10 w-35 px-6  text-sm tracking-wide hover:shadow-[0_0_25px_rgba(192,38,211,0.6)] transition-all uppercase">
-      {props.text}
-      {props.icon && <props.icon size={18} />} 
-      
-    </button>
-  );
-}
 
-function Secondary(props){
-   return (
-    <button onClick={props.onClick} className="cursor-pointer rounded-full h-10 w-35 px-6 glass-card text-sm tracking-wide hover:bg-white/10 hover:border-primary/50 transition-all uppercase">
+  const base = "cursor-pointer rounded-full h-10 w-35  px-6 py-3 uppercase transition-all duration-300 text-sm tracking-wide flex items-center justify-center gap-2 ";
+
+  const styles = {
+    primary:"bg-primary hover:shadow-[0_0_25px_rgba(192,38,211,0.6)]",
+    secondary:"glass-card  hover:bg-white/10 hover:border-primary/50"
+  };
+
+  return (
+    <button onClick={props.onClick} className={`${base} ${styles[props.variant]}`}>
       {props.text}
-      {props.icon && <props.icon size={18} />} 
+      {props.icon && <props.icon size={13} />} 
       
     </button>
   );
 }
 
 export default Button;
-export  {Secondary}
 
 
 // Render the icon if it exists, with a size of 18
