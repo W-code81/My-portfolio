84ff7d57e3d0dcff4328dfb2756b1976f8e58643
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-04-26 23:18:37 +0100

    feat:     -added custom padding class     -added hover scale class     -increased btn size     -updated secondary btn style

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,16 +1,16 @@
 function Button(props) {
 
-  const base = "cursor-pointer rounded-full h-10 w-35  px-6 py-3 uppercase transition-all duration-300 text-sm tracking-wide flex items-center justify-center gap-2 ";
+  const base = "padding cursor-pointer rounded-full h-10 w-fit px-10 py-3 uppercase transition-all duration-300 text-sm tracking-wide flex items-center justify-center gap-3 whitespace-nowrap hover:scale-105";
 
   const styles = {
     primary:"bg-primary hover:shadow-[0_0_25px_rgba(192,38,211,0.6)]",
-    secondary:"glass-card  hover:bg-white/10 hover:border-primary/50"
+    secondary:"bg-white/5 backdrop-blur-md border border-white/20 text-white hover:bg-white/10 hover:border-primary/50"
   };
 
   return (
     <button onClick={props.onClick} className={`${base} ${styles[props.variant]}`}>
       {props.text}
-      {props.icon && <props.icon size={13} />} 
+      {props.icon && <props.icon size={16} />} 
       
     </button>
   );
