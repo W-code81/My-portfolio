ed74bf0c6e7c0af3ff5b9f0940f62bedc9c370cf
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-01 16:20:05 +0100

    feat: imported socialLink and Navbar component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
 import Hero from "./Hero";
 import AboutTeaser from "./AboutTeaser";
+import SocialLinks from "./SocialLinks";
+import Navbar from "./Navbar";
 
 function Home(){
     return(
         <div>
+            <Navbar />
             <Hero />
             <AboutTeaser />
+            <SocialLinks />
         </div>
     )
 }
