f88f1a1d6b03115d4ce0b6d33e505eadf4ac4c97
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-04 00:22:23 +0100

    feat: optimized for mobile first

--- src/components/AboutTeaser.jsx
+++ src/components/AboutTeaser.jsx
@@ -2,19 +2,19 @@ import TerminalCard from './TerminalCard';
 
 function AboutTeaser() {
   return (
-    <section  className="flex flex-row items-center justify-center min-h-screen py-16 px-32 bg-bg-dark relative overflow-hidden">
+    <section className="flex flex-row items-center justify-center min-h-screen bg-bg-dark relative overflow-hidden">
       
       {/* ambient gradients */}
       <div className="absolute top-0 right-0 w-1/2 h-full bg-linear-to-l from-primary/5 to-transparent pointer-events-none" />
       <div className="absolute bottom-0 left-0 w-full h-1/2 bg-linear-to-t from-primary/5 to-transparent pointer-events-none" />
 
-      <div className="max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-2  gap-8 md:gap-12 items-center relative z-10">
+      <div className="max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 md:gap-12 items-center relative z-10">
         
         {/* Left col — text */}
-        <div style={{margin:"24px 0 48px"}} className="order-1 lg:order-1 flex flex-col gap-6">
+        <div className="order-1 lg:order-1 flex flex-col gap-6 mx-4 md:mx-0">
           
           {/* section label */}
-          <div  className="flex items-center gap-2">
+          <div  className="flex items-center gap-2 mt-4 md:mt-0">
             <span className="h-px w-8 bg-primary shadow-neon-sm" />
             <span className="text-primary tracking-widest text-sm uppercase text-glow neon-text">
               About Me
@@ -28,7 +28,7 @@ function AboutTeaser() {
           </h2>
 
           {/* bio */}
-          <p className="text-gray-300 text-lg leading-relaxed font-light hero-text">
+          <p className="text-gray-300 text-sm md:text-lg leading-relaxed font-light">
             Hey, I'm Aderogba Oluwadamilola John — a 19 year old Nigerian
             developer and UI/UX designer with 2.5 years of experience. 
             My goal is to build scalable, user-friendly web applications 
@@ -36,7 +36,7 @@ function AboutTeaser() {
           </p>
 
           {/* stats */}
-          <div  style={{paddingLeft:"24px"}}  className="flex flex-col gap-4 border-l-2 border-primary/30 pl-6">
+          <div className="flex item flex-col gap-4 border-l-2 border-primary/30 pl-3 md:pl-6 mt-2 md:mt-0 mb-3 md:mb-0">
             <div>
               <h4 className="text-white text-lg">Experience</h4>
               <p className="text-sm text-gray-500">2.5 Years in Development & Design</p>
