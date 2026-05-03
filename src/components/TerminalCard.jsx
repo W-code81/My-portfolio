4ad8876dd7d05d61ff7be8e3cd0eef042cdc77c4
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-04 00:56:33 +0100

    feat: optimized for mobile first

--- src/components/TerminalCard.jsx
+++ src/components/TerminalCard.jsx
@@ -1,50 +1,52 @@
 import Button from "./Btn";
-import {User} from "lucide-react";
+import { User } from "lucide-react";
 
 const Info = [
-  { text: "More on Me", icon: User, variant: "secondary"}
+  { text: "More on Me", icon: User, variant: "secondary" }
 ]
 
 function TerminalCard() {
   return (
     <div>
 
-      <div className="glass-card rounded-4xl overflow-hidden border border-primary/20 font-mono text-xs text:sm neon-sm">
+      <div className="glass-card rounded-4xl overflow-hidden border border-primary/20 font-mono text-sm neon-sm mx-4 md:mx-0">
 
         {/* Terminal header */}
-        <div className="padding bg-card-darker px-3 py: md:px-4 md:py-3 flex items-center gap-2 border-b border-white/10">
-          <div className="w-3 h-3 rounded-full bg-[#ff5f56]" />
-          <div className="w-3 h-3 rounded-full bg-[#ffbd2e]" />
-          <div className="w-3 h-3 rounded-full bg-[#27c93f]" />
-          <div className="flex-1 text-center text-gray-500 text-xs">
+        <div className="bg-card-darker px-4 py-2 md:px-5 md:py-3 flex items-center gap-2 border-b border-white/10">
+          <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-[#ff5f56]" />
+          <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-[#ffbd2e]" />
+          <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-[#27c93f]" />
+          <div className="flex-1 text-center text-gray-500 text-sm md:text-xs">
             user@portfolio:~
           </div>
         </div>
 
         {/* Terminal body */}
-        <div className="p-4 md:p-6 text-gray-300 space-y-3 md:space-y-4 bg-bg-dark/90">
-          <div style={{ padding: "20px 0 5px 15px" }} className="flex gap-2 ">
+        <div className="text-gray-300 space-y-2 md:space-y-4 bg-bg-dark/90">
+          <div className="flex gap-2 pt-5 pb-1.25 pl-3.75">
             <span className="text-primary">➜</span>
             <span className="text-purple-400">~</span>
             <span>whoami</span>
           </div>
-          <div style={{ padding: "0 12px", margin: "8px 12px" }} className=" text-gray-400 space-y-1">
+
+          <div className="px-3 my-2 mx-3 text-gray-400 space-y-1">
             <p>&gt; Nigerian Developer & UI/UX Designer</p>
             <p>&gt; 19 years old</p>
             <p>&gt; 2.5 years experience</p>
           </div>
 
-          <div style={{ padding: "10px 15px" }} className="flex gap-2">
+          <div className="py-2.5 px-3.75 flex gap-2">
             <span className="text-primary">➜</span>
             <span className="text-purple-400">~</span>
             <span>cat mission.txt</span>
           </div>
-          <div style={{ padding: "0 12px", margin: "8px 12px" }} className="pl-4 text-[#ce9178]">
+          
+          <div className="px-3 my-2 mx-3 text-[#ce9178]">
             "Create scalable, user-friendly web applications
             that guide users in achieving their goals."
           </div>
 
-          <div style={{ padding: "10px 15px" }} className="flex gap-2">
+          <div className="py-2.5 px-3.75 flex gap-2">
             <span className="text-primary">➜</span>
             <span className="text-purple-400">~</span>
             <span className="animate-pulse">_</span>
@@ -53,9 +55,9 @@ function TerminalCard() {
       </div>
 
       {/* More About ME -> main about page */}
-      <div style={{margin:"24px 0"}} className="hidden lg:inline-flex items-center justify-start neon-sm rounded-full">
+      <div className="mt-6 hidden lg:inline-flex items-center justify-start neon-sm rounded-full">
         {Info.map(btn =>
-          <Button key={btn.text} text={btn.text} variant={btn.variant} icon={btn.icon}/>
+          <Button key={btn.text} text={btn.text} variant={btn.variant} icon={btn.icon} />
         )}
       </div>
     </div>
