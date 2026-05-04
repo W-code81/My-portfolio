e4f730f5c2abcbba842d29da1a1a5980097ddcf7
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-04 18:51:49 +0100

    feat: added workTeaser to home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import Hero from "./Hero";
 import AboutTeaser from "./AboutTeaser";
 import SocialLinks from "./SocialLinks";
 import Navbar from "./Navbar";
+import WorkTeaser from "./WorkTeaser";
 
 function Home(){
     return(
         <div>
             <Navbar />
-            <Hero />
+            <Hero />  
+            <WorkTeaser />
             <AboutTeaser />
             <SocialLinks />
         </div>
