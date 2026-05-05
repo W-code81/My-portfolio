752140dbedc969277afccc50df3440998a735286
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-05 11:20:28 +0100

    feat: added social link data

---
+++ src/data/socialLinks.js
@@ -0,0 +1,24 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { VscGithubAlt, VscMail }   from "react-icons/vsc";
+import { FiLinkedin } from "react-icons/fi";
+
+const socialLinks = [
+  {
+    href: "https://github.com/w-code81",
+    icon: VscGithubAlt 
+  },
+  {
+    href: "https://www.linkedin.com/in/aderogba-oluwadamilola-64066a36a",
+    icon: FiLinkedin
+  },
+  {
+    href: "https://x.com/wcode81",
+    icon: FaXTwitter 
+  },
+  {
+    href: "mailto:aderogbaoluwadamilola@gmail.com",
+    icon: VscMail
+  }
+];
+
+export default socialLinks;
