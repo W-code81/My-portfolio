caa4340457e9df16a5db94f13dc21056c85af768
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-07 19:47:32 +0100

    feat: removed nav form home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Hero from "../sections/Hero";
 import AboutTeaser from "../sections/AboutTeaser";
-import Navbar from "../components/Navbar";
 import WorkTeaser from "../sections/WorkTeaser";
 import Contacts from "../sections/Contacts";
 
 function Home(){
     return(
         <div>
-            <Navbar />
             <Hero />  
             <AboutTeaser />
             <WorkTeaser />
