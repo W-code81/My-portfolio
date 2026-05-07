1b78697ef45fc32ee5e8ca64aa43af57ce3a7abc
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-07 19:42:38 +0100

    feat:     -added new pages     -restructured pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Home from "./pages/Home";
+import AboutMain from "./pages/AboutMain";
+import Resume from "./pages/Resume";
+import Github from "./pages/Github";
+import Navbar from "./components/Navbar";
+
+// import { useState } from "react";
 
 
-function App(){
-    return(
-        <div>
-           <Home />
-        </div>
-        
+function App() {
+    return (
+        <>
+            <Navbar/>
+
+            <main>
+                <Home />
+                {/* <AboutMain />/ */}
+                {/* <Resume /> */}
+                {/* <Github /> */}
+            </main>
+        </>
     )
 }
 
