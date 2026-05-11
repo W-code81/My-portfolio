2f28eea01a6ef9636d4b73b4e9198e10534eb5c7
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-11 14:05:27 +0100

    feat: added button functionality

--- src/data/btnInfo.js
+++ src/data/btnInfo.js
@@ -1,8 +1,8 @@
 import { MoveDown, Download } from "lucide-react";
 
  const btnInfo = [
-  { text: "Contact Me", icon: MoveDown, variant:"primary", onClick: () => alert("Contact me clicked!") },
-  { text: "Download CV", icon: Download, variant:"secondary", onClick: () => alert("Download resume clicked!") },
+  { text: "Contact Me", icon: MoveDown, variant:"primary", onClick: () => document.getElementById("contact").scrollIntoView({ behavior: "smooth" }) },
+  { text: "Download CV", icon: Download, variant:"secondary", onClick: () => window.open("/pdf/My_Resume.pdf" , "_blank")},
 ];
 
 export default btnInfo;
