078ef0e642f28e84c1994c05e27235a0c45a58d3
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-13 07:27:32 +0100

    feat: added stack icon data

---
+++ src/data/stackIcons.js
@@ -0,0 +1,115 @@
+ const frontendStack = [
+    {
+        name: "HTML",
+        src: "/images/stack-icons/html5-original.svg"
+    },
+    {
+        name: "CSS",
+        src: "/images/stack-icons/css3-original.svg"
+    },
+    {
+        name: "Bootstrap",
+        src: "/images/stack-icons/bootstrap-original.svg"
+    },
+    {
+        name: "Tailwind CSS",
+        src: "/images/stack-icons/tailwindcss-original.svg"
+    },
+    {
+        name: "JavaScript",
+        src: "/images/stack-icons/javascript-original.svg"
+    },
+    {
+        name: "JQuery",
+        src: "/images/stack-icons/jquery-original.svg"
+    },
+    {
+        name: "EJS",
+        src: "/images/stack-icons/ejs.svg",
+        class:"invert"
+    },
+    {
+        name: "React",
+        src: "/images/stack-icons/react-original.svg"
+    }
+];
+
+const backendStack = [
+    {
+        name: "Node.js",
+        src: "/images/stack-icons/nodejs-original.svg"
+    },
+    {
+        name: "Express.js",
+        src: "/images/stack-icons/express-original.svg",
+        class:"invert"
+    },
+    {
+        name: "MongoDB",
+        src: "/images/stack-icons/mongodb-original.svg"
+    },
+    {
+        name: "Mongoose",
+        src: "/images/stack-icons/mongoose-original.svg"
+    },
+    {
+        name: "MySQL",
+        src: "/images/stack-icons/mysql-original.svg"
+    },
+    {
+        name: "RESTful APIs",
+        src: "/images/stack-icons/rest.svg"
+    },
+    {
+        name: "OAuth",
+        src: "/images/stack-icons/oauth-original.svg"   
+    },
+    {
+        name: "Passport.js",
+        src: "/images/stack-icons/passport-original.svg"
+    }
+];
+
+const otherStack = [
+    {
+        name: "Git",
+        src: "/images/stack-icons/git-original.svg"
+    },
+    {
+        name: "GitHub",
+        src: "/images/stack-icons/github-original.svg",
+        class:"invert"
+    },
+    {
+        name: "VS Code",
+        src: "/images/stack-icons/vscode-original.svg"
+    },
+    {
+        name: "Figma",
+        src: "/images/stack-icons/figma-original.svg"
+    },
+    {
+        name: "Postman",
+        src: "/images/stack-icons/postman-original.svg"
+    },
+    {
+        name: "Vite.js",
+        src: "/images/stack-icons/vitejs-original.svg"
+    },
+    {
+        name: "Babel",
+        src: "/images/stack-icons/babel-original.svg"
+    },
+    {
+        name: "Nodemon",
+        src: "/images/stack-icons/nodemon-original.svg"
+    },
+    {
+        name: "Vercel",
+        src: "/images/stack-icons/vercel-original.svg",
+        class:"invert"
+    }
+    
+];
+
+export { frontendStack, backendStack, otherStack };
