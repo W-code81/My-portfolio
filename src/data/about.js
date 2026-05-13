c1687c302fc7a1d54bebea46c6e773fb49a3f17f
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-13 10:42:00 +0100

    feat: added about data

---
+++ src/data/about.js
@@ -0,0 +1,27 @@
+import { FaUserAstronaut } from 'react-icons/fa';
+import { Footprints, Drill, Brain } from 'lucide-react';
+
+const about = [
+    {
+      icon: Footprints,
+      title: 'My Journey',
+      description: 'My love for technology started when I got my first laptop. It made me feel capable of creating things and solving problems in ways I never imagined.',
+    },
+    {
+      icon: Drill,
+      title: 'What I Enjoy Building',
+      description: 'I enjoy projects that challenge me to think deeply — from frontend interactions to backend systems. Understanding how things work matters just as much to me as building them.',
+    },
+    {
+      icon: FaUserAstronaut,
+      title: 'Beyond Coding',
+      description: 'Outside development, I enjoy gaming, video editing, photography, videography, and chess. Creativity influences the way I approach both design and problem-solving.',
+    },
+    {
+      icon: Brain,
+      title: 'Mindset',
+      description: 'One of the biggest challenges in my journey has been overcoming imposter syndrome and learning that growth comes from consistency, curiosity, and persistence.',
+    },
+  ];
+
+export default about;
