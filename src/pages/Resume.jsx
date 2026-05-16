57b4481925811797960d438296043dc18b284aad
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-16 23:41:46 +0100

    feat: moved data to a seperated file

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,38 +1,6 @@
 import Button from "../components/Btn";
 import { Download, Briefcase ,GraduationCap } from "lucide-react";
-
-const experience = [
-    {
-        title: "Frontend Development",
-        company: "Self-Learning & Personal Projects",
-        period: "2023 - Present",
-        description:
-            "Built responsive websites and modern React applications while learning frontend architecture, component systems, and UI design principles.",
-    },
-
-    {
-        title: "Fullstack Exploration",
-        company: "MERN Stack Learning",
-        period: "2024 - Present",
-        description:
-            "Currently expanding into backend development using Node.js, Express, MongoDB, and REST APIs while building real-world projects.",
-    },
-
-    {
-        title: "Creative & Visual Design",
-        company: "Video Editing & UI/UX",
-        period: "2024 - Present",
-        description:
-            "Developed creative problem-solving skills through video editing, interface design, branding concepts, and visual storytelling.",
-    },
-];
-const education = [
-    {
-        degree: "Bachelor of Science in Computer Science",
-        school: "University of Port Harcourt",
-        period: "2023 - present",
-    },
-];
+import { education, experience } from "../data/resume";
 
 function Resume() {
     return (
