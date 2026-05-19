b73b3ba49b0c45947d57e420de06a5956125ce10
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-19 12:50:43 +0100

    updated page to use state and pass data to navbar props

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import AboutMain from "./pages/AboutMain";
 import Resume from "./pages/Resume";
 import Github from "./pages/Github";
 import Navbar from "./components/Navbar";
-
-// import { useState } from "react";
+import { useState } from "react";
 
 
 function App() {
+    const [section, setSection] = useState("github")
+    
     return (
         <>
-            <Navbar/>
+            <Navbar active={section} setActive={setSection} />
 
             <main>
-                <Home />
-                {/* <AboutMain />/ */}
-                {/* <Resume /> */}
-                {/* <Github /> */}
+                {section==="home" && <Home /> }
+                {section==="about" && <AboutMain /> }
+                {section==="resume" && <Resume /> }
+                {section==="github" && <Github />}
             </main>
         </>
     )
