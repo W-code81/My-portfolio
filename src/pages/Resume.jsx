cb45e4216ede28863f281bc7a3d73de036697677
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-23 20:56:25 +0100

    feat: removed borders

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -16,7 +16,7 @@ function Resume() {
             {/* Right glow — behind education cards */}
             <div className="absolute bottom-1/3 right-0 w-62.5 h-75  bg-primary/8 rounded-full blur-[100px] pointer-events-none" />
 
-            <section className="border flex flex-col items-center gap-4 relative z-10  w-full max-w-4xl mx-auto px-6">
+            <section className="flex flex-col items-center gap-4 relative z-10  w-full max-w-4xl mx-auto px-6">
 
 
                 <div className="flex flex-col text-center items-center gap-6 my-12">
@@ -86,14 +86,14 @@ function Resume() {
                     whileInView={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.5, delay: 0.2 }}
                     viewport={{ once: true }}>
-                    <div className="border flex items-center gap-3 my-8">
+                    <div className="flex items-center gap-3 my-8">
                         <div className="md:w-10 md:h-10 w-12 h-12 bg-linear-to-r from-fuchsia-500 to-purple-500 rounded-2xl flex items-center justify-center shrink-0">
                             <GraduationCap className="w-6 h-6 text-white" />
                         </div>
                         <h3 className="text-3xl font-bold text-white">Education</h3>
                     </div>
 
-                    <div className="border grid grid-cols-1">
+                    <div className="grid grid-cols-1">
                         {education.map((job, index) => (
                             <motion.div
                                 initial={{ opacity: 0, y: 20 }}
